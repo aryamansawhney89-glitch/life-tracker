7a03c729ca0bee73009721ed4f7f8a7053f4f4c4
aryamansawhney89-glitch <aryamansawhney89@gmail.com>
2026-03-25 12:49:14 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,39 @@
+const CACHE_NAME = "life-tracker-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon.png"
+];
+
+/* INSTALL */
 self.addEventListener("install", event => {
+  self.skipWaiting(); // activate immediately
+
   event.waitUntil(
-    caches.open("app-cache").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./icon.png"
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
 });
 
+/* ACTIVATE */
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key); // delete old caches
+          }
+        })
+      );
+    })
+  );
+});
+
+/* FETCH */
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
